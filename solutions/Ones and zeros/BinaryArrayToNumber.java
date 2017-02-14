2390576c42e485bc2a0689de95ac279e7c315799
Sebastián Uriarte Güimil <sebastian.uriarteg@gmail.com>
2017-02-14 20:16:37 -0300

    Add solution for the "Ones and zeros" problem

---
+++ solutions/Ones and zeros/BinaryArrayToNumber.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+public class BinaryArrayToNumber {
+
+    public static int ConvertBinaryArrayToInt(List<Integer> binary) {
+        return binary.stream().reduce((x, y) -> x * 2 + y).get();
+    }
+}
